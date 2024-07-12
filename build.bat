48d7d68f97af606dca4a1f35c0e7cfbccfbe4c96
Fuad Hasan <fuad.cs22@gmail.com>
2024-07-12 13:08:53 +0600

    scripts

--- build.bat
+++ build.bat
@@ -1,2 +1,7 @@
+@echo off
+REM Build script to compile the Go program into an executable
+echo Building resources.syso...
 windres -o resources.syso resources.rc
+echo Building piarch.exe...
 go build -o piarch.exe
+echo Build complete.
