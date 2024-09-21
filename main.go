588ef7e3d65ffa744e196c828af92e4650bfc987
Fuad Hasan <fuad.cs22@gmail.com>
2024-09-21 14:50:20 +0600

    Refactor main.go, remove unused code and update comments

--- main.go
+++ main.go
@@ -1,25 +1,8 @@
 package main
 
-import (
-	"time"
-	
-	"file-compressor/utils"
-)
-
 
 
 func main() {
 	//test files path '/test'
 
-	startTime :=  time.Now()
-
-	var err error
-
-
-	if err != nil {
-		utils.ColorPrint(utils.RED, err.Error() + "\n")
-	} else {
-		endTime := time.Now()
-		utils.ColorPrint(utils.GREEN, "Time taken: " + utils.TimeTrack(startTime, endTime) + "\n")
-	}
 }
